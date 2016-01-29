d4260d6ef60e4425b4958b575a900856739a8c91
Hugh Francis <me@hughfrancis.me>
2016-01-29 11:22:01 -0500

    Add link model

---
+++ api/db/migrate/20160129161957_create_links.rb
@@ -0,0 +1,13 @@
+class CreateLinks < ActiveRecord::Migration
+  def change
+    create_table :links do |t|
+      t.string :title
+      t.string :description
+      t.string :url, unique: true
+      t.string :image_url
+      t.string :sender
+
+      t.timestamps null: false
+    end
+  end
+end
