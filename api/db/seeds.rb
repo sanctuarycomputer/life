41a1e220741735d36289fa66bf5f8f15411311ed
Hugh Francis <me@hughfrancis.me>
2016-01-29 11:32:56 -0500

    Add Seed Data

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Link.create({
+  title: 'Kinto',
+  description: 'A lightweight JSON storage service with synchronisation and sharing abilities.',
+  url: 'http://kinto.readthedocs.org/en/latest/',
+  image_url: 'http://kinto.readthedocs.org/en/latest/_images/logo.svg',
+  sender: 'malina@sanctuary.computer'
+})
+
+
+Link.create({
+  title: 'How I fixed Atom',
+  description: 'When good regexes go bad',
+  url: 'http://davidvgalbraith.com/how-i-fixed-atom/',
+  image_url: 'http://1.gravatar.com/avatar/700886ec68221f3fb1793207c291f519?s=98&d=mm&r=g',
+  sender: 'sebastian@sanctuary.computer'
+})
+
+Link.create({
+  title: 'The Man Who Would Tame Cancer',
+  description: 'Patrick Soon-Shiong is opening a new front in the war on the deadly disease.',
+  url: 'http://nautil.us/issue/32/space/the-man-who-would-tame-cancer',
+  image_url: 'http://static.nautil.us/8237_7dd2ae7db7d18ee7c9425e38df1af5e2.png',
+  sender: 'hugh@sanctuary.computer'
+})
